cd0a418a846916a00a8570296bfa91501eb0b66c
ABHISHEK NAYAK <66876321+Abhisheknayak-programer@users.noreply.github.com>
2021-09-09 13:23:08 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-const NavFunctions = () => {
-    const nav = document.querySelector('.nav-links');
-    const btn = document.querySelector('.burger');
-    const navLinks = document.querySelectorAll('.nav-links li');
-
-    btn.addEventListener('click',(()=>{
-        //  Nav Toggler 
-        nav.classList.toggle('nav-active');
-        console.log("Abhishek");
-
-        // HamBurger Toggler
-        btn.classList.toggle('toggle')
-
-
-    }))
-
-}
-
-NavFunctions();
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelector(".nav__links");
+burger.addEventListener("click", function () {
+  navLinks.classList.toggle("navBarActive");
+  burger.classList.toggle("toggle");
+});
